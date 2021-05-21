daf559086a64c82eb3ce986ec67e52977c2d0cb4
yuta.matsui <y.matsui@deepfunk45.com>
2021-05-21 15:50:06 +0900

    up

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cb3d25c7bd3b14383e8a"; }
+/******/ 		__webpack_require__.h = function() { return "15b371a49f63ad56dc87"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,11 @@ import Image from 'next/image'
 
 const RootPage = () => {
     return (
+        <>
+        <h1>Yuta Matsui Photo Archive</h1>
         <div>
-            <Image src={"/01.jpg"} width={1920} height={1281} />
-            <Image src={"/02.jpg"} width={1920} height={1281} />
+            <Image src={"/01.jpg"} width={1920} height={1281} priority={true} />
+            <Image src={"/02.jpg"} width={1920} height={1281} priority={true} />
             <Image src={"/03.jpg"} width={1920} height={1281} />
             <Image src={"/04.jpg"} width={1920} height={1281} />
             <Image src={"/05.jpg"} width={1920} height={1281} />
@@ -15,6 +17,8 @@ const RootPage = () => {
             <Image src={"/10.jpg"} width={1281} height={1920} />
             <Image src={"/11.jpg"} width={1920} height={1281} />
         </div>
+        </>
+        
     );
 };
 
